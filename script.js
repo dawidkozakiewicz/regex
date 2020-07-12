bb2cb7e8e9cac8c4d6c5ad87e67110049912a54c
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-07-12 22:49:11 +0200

    global and case ignore

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 {
-    let myString = "Hello, World!";
-    let myRegex = /Hello/;
-    let result = /Hello/.test(myString);
-    console.log(result);
+  let myString = "Hello, World!";
+  let myRegex = /Hello/;
+  let result = /Hello/.test(myString);
+  console.log(result);
 }
 
 {
-    let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
-    let waldoRegex = /wALDO/;
-    let result = waldoRegex.test(waldoIsHiding);
-    console.log(result)
+  let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+  let waldoRegex = /wALDO/;
+  let result = waldoRegex.test(waldoIsHiding);
+  console.log(result);
 }
 
 {
-    let petString = "James has a pet cat.";
-    let petRegex = /John|dog|bird|Liz|cat|Alice|fish/;
-    let result = petRegex.test(petString);
-    console.log(result);
+  let petString = "James has a pet cat.";
+  let petRegex = /John|dog|bird|Liz|cat|Alice|fish/;
+  let result = petRegex.test(petString);
+  console.log(result);
 }
 
 {
-    let myString = "freeCodeCamp";
-    let fccRegex = /freecodecamp/i;
-    let result = fccRegex.test(myString);
-    console.log(result);
+  let myString = "freeCodeCamp";
+  let fccRegex = /freecodecamp/i;
+  let result = fccRegex.test(myString);
+  console.log(result);
 }
 
 {
-    let extractStr = "Extract the word 'coding' from this string.";
-    let codingRegex = /coding/;
-    let result = extractStr.match(codingRegex);
-    console.log(result);
+  let extractStr = "Extract the word 'coding' from this string.";
+  let codingRegex = /coding/;
+  let result = extractStr.match(codingRegex);
+  console.log(result);
+}
+
+{
+  let twinkleStar = "Twinkle, twinkle, little star";
+  let starRegex = /Twinkle/gi;
+  let result = twinkleStar.match(starRegex);
+  console.log(result);
 }
