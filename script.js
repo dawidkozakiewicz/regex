eee6fcb74da28f8554f52f474d5edb8ba8bd4624
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-07-15 22:08:44 +0200

    find single characters

--- script.js
+++ script.js
@@ -46,3 +46,11 @@
   let result = unRegex.test(exampleStr);
   console.log(result);
 }
+
+{
+  let quoteSample =
+    "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+  let vowelRegex = /[aeiou]/gi;
+  let result = quoteSample.match(vowelRegex);
+  console.log(result);
+}
