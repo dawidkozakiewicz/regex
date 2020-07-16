2fb49bcb83bc279419542814873ce0a4a8661ca9
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-07-16 20:30:58 +0200

    range of letters

--- script.js
+++ script.js
@@ -54,3 +54,10 @@
   let result = quoteSample.match(vowelRegex);
   console.log(result);
 }
+
+{
+  let quoteSample = "The quick brown fox jumps over the lazy dog.";
+  let alphabetRegex = /[a-z]/gi;
+  let result = quoteSample.match(alphabetRegex);
+  console.log(result);
+}
