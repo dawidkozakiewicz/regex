cc25bfe5c1772a0dd69f708e7844aaf93855d161
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-07-17 23:07:03 +0200

    range of letters and numbers

--- script.js
+++ script.js
@@ -61,3 +61,9 @@
   let result = quoteSample.match(alphabetRegex);
   console.log(result);
 }
+{
+  let quoteSample = "Blueberry 3.141592653s are delicious.";
+  let myRegex = /[h-s2-6]/gi;
+  let result = quoteSample.match(myRegex);
+  console.log(result);
+}
