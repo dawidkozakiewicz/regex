c59628d4e5fcfbef7d2a31164834c3e37f557603
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-07-26 19:48:17 +0200

    Match Beginning String Patterns

--- script.js
+++ script.js
@@ -109,3 +109,10 @@
   let matchedCriminals = crowd.match(reCriminals);
   console.log(matchedCriminals);
 }
+
+{
+  let rickyAndCal = "Cal and Ricky both like racing.";
+  let calRegex = /^Cal/;
+  let result = calRegex.test(rickyAndCal);
+  console.log(result);
+}
