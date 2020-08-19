dee267aed16e4e392412a543d243b0236982b3c7
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-08-19 22:41:40 +0200

    Match Non-Whitespace Characters

--- script.js
+++ script.js
@@ -163,3 +163,10 @@
   let result = sample.match(countWhiteSpace);
   console.log(result);
 }
+
+{
+  let sample = "Whitespace is important in separating words";
+  let countNonWhiteSpace = /\S/g;
+  let result = sample.match(countNonWhiteSpace);
+  console.log(result);
+}
