07d44e433a7d1358cb432acbd181ebed28b6edd0
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-08-21 19:44:01 +0200

    Specify Only the Lower Number of Matches

--- script.js
+++ script.js
@@ -177,3 +177,10 @@
   let result = ohRegex.test(ohStr);
   console.log(result);
 }
+
+{
+  let haStr = "Hazzzzah";
+  let haRegex = /Haz{4,}ah/;
+  let result = haRegex.test(haStr);
+  console.log(result);
+}
