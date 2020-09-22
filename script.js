c0ec0f32794d297fee5bdecaf855acc0a45fbe41
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-09-22 22:49:52 +0200

    comment 17

--- script.js
+++ script.js
@@ -118,7 +118,7 @@
 }
 
 {
-  let caboose = "The last car on a train is the caboose";
+  let caboose = "The last car on a train is the caboose"; // Match Ending String Patterns
   let lastRegex = /caboose$/;
   let result = lastRegex.test(caboose);
   console.log(result);
